943e6e6132d5b726788bd2623e8114f559967838
ktg8377 <ktg8377@naver.com>
2023-06-15 17:16:20 +0900

    구구단 2단 완성

--- Gugudan/src/ezen/Gugudan.java
+++ Gugudan/src/ezen/Gugudan.java
@@ -5,10 +5,13 @@ public class Gugudan {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("구구단 연습문제");
+		System.out.println("구구단 2단");
 		
-		System.out.println(2 * 1);
-		System.out.println(2 * 9);
+		int dan = 2;
+		
+		for (int i = 1; i < 10; i++) {
+			System.out.println(dan + " * " + i + " = " + dan * i);
+		}
 		
 	}
 
