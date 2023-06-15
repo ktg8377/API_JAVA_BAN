b93d1a031dd9a0ee0cbfdfedefb6edd43c4293e5
ktg8377 <ktg8377@naver.com>
2023-06-15 19:04:56 +0900

    dsfsdaf

--- Gugudan/src/ezen/Gugudan.java
+++ Gugudan/src/ezen/Gugudan.java
@@ -16,10 +16,7 @@ public class Gugudan {
 		
 		System.out.println("1");
 		System.out.println("3");
-		System.out.println("5");
-		System.out.println("7");
-		System.out.println("9");
-		System.out.println("11");
+		
 	}
 
 }
